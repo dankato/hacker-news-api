0bacd30173a3e0f0ae31811393b3d025c6b0ea1c
krp312 <krp312@me.com>
2017-07-07 17:14:47 -0400

    still working on POST

--- server.js
+++ server.js
@@ -8,13 +8,36 @@ const { DATABASE, PORT } = require('./config');
 
 const app = express();
 
+// const dotenv = require('dotenv');
+
+let knex = require('knex')(DATABASE);
+
 app.use(morgan(':method :url :res[location] :status'));
 
-app.use(bodyParser.json());
+const jsonParser = bodyParser.json();
 
 app.get('/', (req, res) => {
+  knex.select()
+    .from('news')
+    .then(res => console.log(res));
+
   res.send('hello world');
-});
+}); 
+
+// Create a POST endpoint, /api/stories, which adds new stories to your database.
+// It should expect a JSON request body containing a title property and a url property
+// It should respond with a 201 Created status and the story ////
+  // The submitted title, URL and the new ID should come back
+  // Plus the URL should be in the location header
+// Sending a votes property should not allow users to cheat the system by setting an arbitrary number of upvotes
+// Test your endpoint by:
+// Using Postman to add some stories
+// Using the shell to make sure they were added to the database
+
+// psql postgres://ikqipnsg:Sl_j_HtbHVTOjEXmBk9VYza5EWqqI9wn@pellefant.db.elephantsql.com:5432/ikqipnsg
+// server pellefant.db.elephantsql.com
+// user/database ikqipnsg
+// password Sl_j_HtbHVTOjEXmBk9VYza5EWqqI9wn
 
 // POST ENDPOINT
 app.post('/api/stories', jsonParser, (req, res) => {
@@ -23,14 +46,19 @@ app.post('/api/stories', jsonParser, (req, res) => {
   for (let i=0; i<requiredFields.length; i++) {
     const field = requiredFields[i];
     if (!(field in req.body)) {
-      const message = `Missing \`${field}\` in request body`
+      const message = `Missing \`${field}\` in request body`;
       console.error(message);
       return res.status(400).send(message);
     }
   }
 
-  const item = news.create(req.body.title, req.body.url);
-  res.status(201).json(item);;
+  knex('news')
+    .insert({'title': req.body.title, 'url': req.body.url})
+    .returning(['id','title', 'url', 'votes'])
+    .then(result => {
+      res.status(201).json(result);
+    })
+    .catch(err => res.status(500).json(err));
 });
 
 // Sending a votes property should not allow users to cheat the system by setting an arbitrary number of upvotes
@@ -41,7 +69,7 @@ app.post('/api/stories', jsonParser, (req, res) => {
 
 
 let server;
-let knex;
+// let knex;
 function runServer(database = DATABASE, port = PORT) {
   return new Promise((resolve, reject) => {
     try {
